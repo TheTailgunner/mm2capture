75cfde0cbfe4cc2d623db15c355762b4fc574dc6
Vladislav <nvladislav9@gmail.com>
2018-01-15 03:50:25 +0700

    Add GPL license and credits into sources

--- src/compress/packer.h
+++ src/compress/packer.h
@@ -1,3 +1,17 @@
+/* By Vladislav Nikolaev nvladislav9 at gmail dot com
+
+This file is part of mm2capture.
+
+mm2capture is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+mm2capture is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. */
+
 #ifndef _PACKER_H
 #define _PACKER_H
 
